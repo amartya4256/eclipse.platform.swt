dc96eb7e3c98be4b56209f4165ac9696ae4e5bba
Andreas Koch <andreas.koch@yatta.de>
2024-02-08 09:02:33 +0100

    Adjust DPI test

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet373.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet373.java
@@ -31,7 +31,8 @@ import org.eclipse.swt.layout.*;
 import org.eclipse.swt.widgets.*;
 
 /**
- * Dynamic DPI with restart example snippet Display current monitors zoom
+ * Dynamic DPI example snippet: Display current monitor handle/zoom and option
+ * to refresh these values.
  *
  * For a list of all SWT example snippets see
  * http://www.eclipse.org/swt/snippets/
@@ -58,6 +59,7 @@ public class Snippet373 {
 		default:
 			path = IMAGE_PATH_100;
 		}
+		System.out.println("Snippet373: ImageFileNameProvider: " + zoom + " : " + path);
 		return path;
 	};
 
@@ -75,49 +77,33 @@ public class Snippet373 {
 		final Image eclipseCTab2 = new Image(display, filenameProvider);
 
 		Shell shell = new Shell(display);
-		shell.setText("Snippet 373");
 		shell.setImage(eclipse);
-		shell.setText("DynamicDPI @ " + DPIUtil.getDeviceZoom());
+		shell.setText("DynamicDPI @ " + DPIUtil.getDeviceZoom() );
 		shell.setLayout(new RowLayout(SWT.VERTICAL));
 		shell.setLocation(100, 100);
-		shell.setSize(500, 600);
-		shell.addListener(SWT.ZoomChanged, new Listener() {
-			@Override
-			public void handleEvent(Event e) {
-				if (display.getPrimaryMonitor().equals(shell.getMonitor())) {
-					MessageBox box = new MessageBox(shell, SWT.PRIMARY_MODAL | SWT.OK | SWT.CANCEL);
-					box.setText(shell.getText());
-					box.setMessage("DPI changed, do you want to exit & restart ?");
-					e.doit = (box.open() == SWT.OK);
-					if (e.doit) {
-						shell.close();
-						System.out.println("Program exit.");
-					}
-				}
-			}
-		});
+		shell.setSize(500, 500);
 
 		// Menu
-		Menu bar = new Menu(shell, SWT.BAR);
-		shell.setMenuBar(bar);
-		MenuItem fileItem = new MenuItem(bar, SWT.CASCADE);
-		fileItem.setText("&File");
+		Menu bar = new Menu (shell, SWT.BAR);
+		shell.setMenuBar (bar);
+		MenuItem fileItem = new MenuItem (bar, SWT.CASCADE);
+		fileItem.setText ("&File");
 		fileItem.setImage(eclipse);
-		Menu submenu = new Menu(shell, SWT.DROP_DOWN);
-		fileItem.setMenu(submenu);
-		MenuItem subItem = new MenuItem(submenu, SWT.PUSH);
-		subItem.addListener(SWT.Selection, e -> System.out.println("Select All"));
-		subItem.setText("Select &All\tCtrl+A");
-		subItem.setAccelerator(SWT.MOD1 + 'A');
+		Menu submenu = new Menu (shell, SWT.DROP_DOWN);
+		fileItem.setMenu (submenu);
+		MenuItem subItem = new MenuItem (submenu, SWT.PUSH);
+		subItem.addListener (SWT.Selection, e -> System.out.println ("Select All"));
+		subItem.setText ("Select &All\tCtrl+A");
+		subItem.setAccelerator (SWT.MOD1 + 'A');
 		subItem.setImage(eclipse);
 
 		// CTabFolder
 		CTabFolder folder = new CTabFolder(shell, SWT.BORDER);
 		for (int i = 0; i < 2; i++) {
 			CTabItem cTabItem = new CTabItem(folder, i % 2 == 0 ? SWT.CLOSE : SWT.NONE);
-			cTabItem.setText("Item " + i);
+			cTabItem.setText("Item "+i);
 			Text textMsg = new Text(folder, SWT.MULTI);
-			textMsg.setText("Content for Item " + i);
+			textMsg.setText("Content for Item "+i);
 			cTabItem.setControl(textMsg);
 			cTabItem.setImage((i % 2 == 1) ? eclipseCTab1 : eclipseCTab2);
 		}
@@ -133,12 +119,12 @@ public class Snippet373 {
 		composite.setLayout(new RowLayout(SWT.HORIZONTAL));
 
 		// Label with Image
-		Label label1 = new Label(composite, SWT.BORDER);
-		label1.setImage(eclipse);
+		Label label1 = new Label (composite, SWT.BORDER);
+		label1.setImage (eclipse);
 
 		// Label with text only
-		Label label2 = new Label(composite, SWT.BORDER);
-		label2.setText("No Image");
+		Label label2 = new Label (composite, SWT.BORDER);
+		label2.setText ("No Image");
 
 		// Button with text + Old Image Constructor
 		Button oldButton1 = new Button(composite, SWT.PUSH);
@@ -154,36 +140,35 @@ public class Snippet373 {
 		createDialog.setText("Child Dialog");
 		createDialog.setImage(eclipse);
 		createDialog.addSelectionListener(new SelectionAdapter() {
-
 			@Override
 			public void widgetSelected(SelectionEvent event) {
-				final Shell dialog = new Shell(shell, SWT.DIALOG_TRIM | SWT.RESIZE);
+				final Shell dialog = new Shell (shell, SWT.DIALOG_TRIM | SWT.RESIZE);
 				dialog.setText("Child Dialog");
-				RowLayout rowLayout = new RowLayout(SWT.VERTICAL);
-				dialog.setLayout(rowLayout);
-				Label label = new Label(dialog, SWT.BORDER);
+				RowLayout rowLayout = new RowLayout (SWT.VERTICAL);
+				dialog.setLayout (rowLayout);
+				Label label = new Label (dialog, SWT.BORDER);
 				label.setImage(eclipse);
 				Point location = shell.getLocation();
 				dialog.setLocation(location.x + 250, location.y + 50);
-				dialog.pack();
-				dialog.open();
+				dialog.pack ();
+				dialog.open ();
 			}
 		});
 
 		// Toolbar with Image
-		ToolBar toolBar = new ToolBar(composite, SWT.FLAT | SWT.BORDER);
-		Rectangle clientArea = shell.getClientArea();
-		toolBar.setLocation(clientArea.x, clientArea.y);
-		for (int i = 0; i < 2; i++) {
+		ToolBar toolBar = new ToolBar (composite, SWT.FLAT | SWT.BORDER );
+		Rectangle clientArea = shell.getClientArea ();
+		toolBar.setLocation (clientArea.x, clientArea.y);
+		for (int i=0; i<2; i++) {
 			int style = i % 2 == 1 ? SWT.DROP_DOWN : SWT.PUSH;
-			ToolItem toolItem = new ToolItem(toolBar, style);
-			toolItem.setImage((i % 2 == 0) ? eclipseToolBar1 : eclipseToolBar2);
+			ToolItem toolItem = new ToolItem (toolBar, style);
+			toolItem.setImage ((i % 2 == 0) ? eclipseToolBar1 : eclipseToolBar2);
 			toolItem.setEnabled(i % 2 == 0);
 		}
-		toolBar.pack();
+		toolBar.pack ();
 
 		Button button = new Button(shell, SWT.PUSH);
-		button.setText("Refresh-Current Monitor : Zoom");
+		button.setText("Refresh-Current Monitor [Handle : Zoom] ");
 		Text text1 = new Text(shell, SWT.BORDER);
 		Monitor monitor = button.getMonitor();
 		text1.setText("" + monitor.getZoom());
@@ -195,55 +180,55 @@ public class Snippet373 {
 			}
 		});
 		Button button2 = new Button(shell, SWT.PUSH);
-		button2.setText("Refresh-Both Monitors : Zoom");
+		button2.setText("Refresh-Both Monitors [Handle : Zoom]");
 		Text text2 = new Text(shell, SWT.BORDER);
 		Monitor[] monitors = display.getMonitors();
-		StringBuilder text2String = new StringBuilder();
+		StringBuffer text2String = new StringBuffer();
 		for (int i = 0; i < monitors.length; i++) {
-			text2String.append(monitors[i].getZoom() + (i < (monitors.length - 1) ? " - " : ""));
+			text2String.append(monitors[i].getZoom()  + (i < (monitors.length -1) ? " - " : ""));
 		}
 		text2.setText(text2String.toString());
 		button2.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseDown(MouseEvent e) {
 				Monitor[] monitors = display.getMonitors();
-				StringBuilder text2String = new StringBuilder();
+				StringBuffer text2String = new StringBuffer();
 				for (int i = 0; i < monitors.length; i++) {
-					text2String.append(monitors[i].getZoom() + (i < (monitors.length - 1) ? " - " : ""));
+					text2String.append(monitors[i].getZoom()  + (i < (monitors.length -1) ? " - " : ""));
 				}
 				text2.setText(text2String.toString());
 			}
 		});
 
 		// Table
-		Table table = new Table(shell, SWT.MULTI | SWT.BORDER | SWT.FULL_SELECTION);
-		table.setLinesVisible(true);
-		table.setHeaderVisible(true);
-		String titles[] = { "Title 1" };
-		for (String title : titles) {
-			TableColumn column = new TableColumn(table, SWT.NONE);
-			column.setText(title);
+		Table table = new Table (shell, SWT.MULTI | SWT.BORDER | SWT.FULL_SELECTION);
+		table.setLinesVisible (true);
+		table.setHeaderVisible (true);
+		String titles[] = {"Title 1"};
+		for (int i=0; i<titles.length; i++) {
+			TableColumn column = new TableColumn (table, SWT.NONE);
+			column.setText (titles [i]);
 			column.setImage(eclipseTableHeader);
 		}
 		for (int i = 0; i < 1; i++) {
-			TableItem item = new TableItem(table, SWT.NONE);
-			item.setText(0, "Data " + i);
+			TableItem item = new TableItem (table, SWT.NONE);
+			item.setText (0, "Data " + i);
 			item.setImage(0, eclipseTableItem);
 		}
-		for (int i = 0; i < titles.length; i++) {
-			table.getColumn(i).pack();
+		for (int i=0; i<titles.length; i++) {
+			table.getColumn (i).pack ();
 		}
 
 		// Tree
-		final Tree tree = new Tree(shell, SWT.BORDER);
-		for (int i = 0; i < 1; i++) {
-			TreeItem iItem = new TreeItem(tree, 0);
-			iItem.setText("TreeItem (0) -" + i);
+		final Tree tree = new Tree (shell, SWT.BORDER);
+		for (int i=0; i<1; i++) {
+			TreeItem iItem = new TreeItem (tree, 0);
+			iItem.setText ("TreeItem (0) -" + i);
 			iItem.setImage(eclipseTree1);
 			TreeItem jItem = null;
-			for (int j = 0; j < 1; j++) {
-				jItem = new TreeItem(iItem, 0);
-				jItem.setText("TreeItem (1) -" + j);
+			for (int j=0; j<1; j++) {
+				jItem = new TreeItem (iItem, 0);
+				jItem.setText ("TreeItem (1) -" + j);
 				jItem.setImage(eclipseTree2);
 				jItem.setExpanded(true);
 			}
