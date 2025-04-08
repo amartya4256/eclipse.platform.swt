bda41692cc182209dff86e246983444fd0966369
Amartya Parijat <amartya.parijat@yatta.de>
2025-04-08 13:47:22 +0200

    Edge:evaluate deadlock fix  #1466
    This commit cotributes to replicating the behaviour of Edge:evaluate as
    it is in WebKit - it must not wait for the execution of script to obtain
    the result, in case evaluate is called inside a WebView callback. This
    commit also makes sure that OpenWindowListeners are execute
    synchronously or asynchronously depending on if the
    handleNewWindowRequested is called from the evaluate script. Moreover,
    this enables all the tests which were failing because of Edge:evaluate
    limitations.
    
    contributes to
    https://github.com/eclipse-platform/eclipse.platform.swt/issues/1771 and https://github.com/eclipse-platform/eclipse.platform.swt/issues/1919

--- bundles/org.eclipse.swt/Eclipse SWT Browser/win32/org/eclipse/swt/browser/Edge.java
+++ bundles/org.eclipse.swt/Eclipse SWT Browser/win32/org/eclipse/swt/browser/Edge.java
@@ -81,6 +81,7 @@ class Edge extends WebBrowser {
 
 	static boolean inCallback;
 	boolean inNewWindow;
+	private boolean inEvaluate;
 	HashMap<Long, LocationEvent> navigations = new HashMap<>();
 	private boolean ignoreGotFocus;
 	private boolean ignoreFocusIn;
@@ -906,11 +907,21 @@ public Object evaluate(String script) throws SWTException {
 	// Feature in WebView2. ExecuteScript works regardless of IsScriptEnabled setting.
 	// Disallow programmatic execution manually.
 	if (!jsEnabled) return null;
-
+	if(inCallback) {
+		// Execute script, but do not wait for async call to complete as otherwise it
+		// can cause a deadlock if execute inside a WebView callback.
+		execute(script);
+		return null;
+	}
 	String script2 = "(function() {try { " + script + " } catch (e) { return '" + ERROR_ID + "' + e.message; } })();\0";
 	String[] pJson = new String[1];
-	int hr = callAndWait(pJson, completion -> webViewProvider.getWebView(true).ExecuteScript(script2.toCharArray(), completion));
-	if (hr != COM.S_OK) error(SWT.ERROR_FAILED_EVALUATE, hr);
+	inEvaluate = true;
+	try {
+		int hr = callAndWait(pJson, completion -> webViewProvider.getWebView(true).ExecuteScript(script2.toCharArray(), completion));
+		if (hr != COM.S_OK) error(SWT.ERROR_FAILED_EVALUATE, hr);
+	} finally {
+		inEvaluate = false;
+	}
 
 	Object data = JSON.parse(pJson[0]);
 	if (data instanceof String && ((String) data).startsWith(ERROR_ID)) {
@@ -1319,7 +1330,7 @@ int handleNewWindowRequested(long pView, long pArgs) {
 	args.GetDeferral(ppv);
 	ICoreWebView2Deferral deferral = new ICoreWebView2Deferral(ppv[0]);
 	inNewWindow = true;
-	browser.getDisplay().asyncExec(() -> {
+	Runnable openWindowHandler = () -> {
 		try {
 			if (browser.isDisposed()) return;
 			WindowEvent openEvent = new WindowEvent(browser);
@@ -1355,7 +1366,21 @@ int handleNewWindowRequested(long pView, long pArgs) {
 			args.Release();
 			inNewWindow = false;
 		}
-	});
+	};
+
+	// In case a new window is opened using evaluate with scripts like
+	// 'window.open()', there can be a deadlock if we execute the handler
+	// asynchronously. And in case a new browser is initialized in the same
+	// environment inside an OpenWindowListener of another browser, there can also
+	// be a deadlock. Hence, generally it should be run asynchronously. However, a
+	// combination of opening a new window from evaluate and starting a new instance
+	// of browser inside the OpenWindowListener should be avoided.
+	if (inEvaluate) {
+		openWindowHandler.run();
+	} else {
+		browser.getDisplay().asyncExec(openWindowHandler);
+	}
+
 	return COM.S_OK;
 }
 
